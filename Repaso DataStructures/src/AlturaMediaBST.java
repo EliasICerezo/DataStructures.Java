8339b2a8d34e8ee91ffc80d6dbdb5bcd812020c9
MagicElyas <elias2603@hotmail.com>
2017-08-01 18:41:10 +0200

    Termino ejercicio AlturaMediaBST

--- Repaso DataStructures/src/AlturaMediaBST.java
+++ Repaso DataStructures/src/AlturaMediaBST.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
 
+import dataStructures.searchTree.BST;
+import dataStructures.searchTree.SearchTree;
 
 public class AlturaMediaBST {
-	
+	public static void main(String [] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("¿Cuantos nodos quieres?: ");
+		int n=sc.nextInt();
+		while(n<0){
+			System.out.print("¿Cuantos nodos quieres?: ");
+			n=sc.nextInt();
+		}
+		
+		
+		Random rnd=new Random();
+		SearchTree<Integer,Integer> st=new BST<>();
+		for(int i=0;i<n;i++){
+			st.insert(rnd.nextInt(1000000), rnd.nextInt(1000000));
+		}
+		System.out.println("La altura media de este arbol es: "+st.height());
+	}
 }
---
+++ Repaso DataStructures/src/AlturaMediaVariosArboles.java
@@ -0,0 +1,6 @@
+
+public class AlturaMediaVariosArboles {
+	public static void main(String [] args){
+		
+	}
+}
